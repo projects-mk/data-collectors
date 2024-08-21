37a689286c5f6ce1002a278b717ae8830f3e92d9
Mateusz Kurdziel <mateuszkurdziel1@gmail.com>
2024-08-21 10:01:46 +0200

    updated secrets retrieval

---
+++ collectors/spiders/companies_data.py
@@ -0,0 +1,34 @@
+import scrapy
+from scrapy.responsetypes import Response
+import random
+import pandas as pd
+import os
+from dotenv import load_dotenv
+from typing import List, Union
+from datetime import datetime, timedelta
+import numpy as np
+import re
+from tenacity import retry, stop_after_attempt, wait_fixed, retry_if_exception_type
+import pytz
+from datetime import datetime
+from utils import user_agents
+
+
+warsaw_tz = pytz.timezone("Europe/Warsaw")
+current_time_warsaw = datetime.now(tz=warsaw_tz).strftime("%d-%m-%Y")
+
+
+load_dotenv()
+conn_str = os.getenv("PG_CONN_STRING")
+
+
+
+
+
+class CompaniesDataSpider(scrapy.Spider):
+    name = "companies-data"
+    allowed_domains = ["biznesradar.pl"]
+    start_urls = ["https://biznesradar.pl"]
+
+    def parse(self, response):
+        pass
--- collectors/spiders/otomoto.py
+++ collectors/spiders/otomoto.py
@@ -6,21 +6,11 @@ import os
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 from lxml import html
+from utils import user_agents, generate_conn_string
 
 load_dotenv()
-conn_str = os.getenv("PG_CONN_STRING")
-user_agents = [
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.5195.52 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:91.0) Gecko/20100101 Firefox/91.0",
-    "Mozilla/5.0 (Linux; Android 11; SM-G965U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Mobile Safari/537.36",
-    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_3_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.3 Safari/605.1.15",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0",
-    "Mozilla/5.0 (Linux; U; Android 11; en-us; SM-G973U Build/RP1A.200720.012) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/91.0.4472.164 Mobile Safari/537.36",
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36",
-    "Mozilla/5.0 (iPhone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1",
-]
+conn_str = generate_conn_string(db="projects")
+
 
 
 class OtomotoSpider(scrapy.Spider):
